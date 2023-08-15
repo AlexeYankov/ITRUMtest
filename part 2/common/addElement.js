3d8bb3b8a9c248a03e008a4a0bff4490d564019d
Alexey Yankov <yankovav.wm@gmail.com>
2023-08-15 12:42:50 +0300

    added some code

---
+++ part 2/common/addElement.js
@@ -0,0 +1,31 @@
+function addElement (value) {
+    const warnInput = document.getElementById("inputWarning");
+    //select main element
+    const mainInput = document.getElementById("input");
+    //create task container
+    const createTaskContainer = document.createElement("div");
+    createTaskContainer.setAttribute("class", "task");
+    const id = crypto.randomUUID();
+    createTaskContainer.setAttribute("id", "div" + id);
+
+    if (!value.trim()) {
+        warnInput.setAttribute("class", "deletedTasksList")
+        return
+    }
+    //set task name value to container
+    const taskName = document.createTextNode(value.trim());
+    createTaskContainer.appendChild(taskName);
+    warnInput.setAttribute("class", "deletedTasksListHidden")
+
+    //create checkbox for task
+    const newCHeckbox = document.createElement("input");
+
+    newCHeckbox.setAttribute("type", "checkbox");
+    newCHeckbox.setAttribute("id", id);
+    createTaskContainer.appendChild(newCHeckbox);
+
+    // add the newly created element and its content into the DOM
+    document.body.insertBefore(createTaskContainer, mainInput);
+    //add listener of checked status
+    newCHeckbox.onclick = () => completeTask(id);
+}
