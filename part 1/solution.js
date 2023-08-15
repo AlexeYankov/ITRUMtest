e794613377d02c74ed0a93248f910d773f79d8ac
Alexey Yankov <yankovav.wm@gmail.com>
2023-08-15 12:44:04 +0300

    added part 1

--- part 1/solution.js
+++ part 1/solution.js
@@ -7,22 +7,46 @@
 
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-var addTwoNumbers = function (l1, l2) {
-    let a = l1,
-        b = l2,
-        c = 0,
-        f = [];
-    if (a.length < 2 || b.length < 2) {
-        c = +a + +b;
-        return c.toString().split("").reverse();
-    }
-    let g = [...a],
-        t = [...b];
-    c = +g.reverse().join("") + +t.reverse().join("");
-    if (c === 0) {
-        return [0];
-    }
-    return c.toString().split("").reverse();
-};
-
-addTwoNumbers([2,4,3], [5,6,4])
+// const addTwoNumbers = function(l1,l2) {
+//     let newNode = new ListNode();
+//     let temp = 0;
+//     const headNode = newNode;
+//     for (let i = 0; i < Infinity;i++) {
+//         let val1 = 0, val2 = 0;
+//         if (l1 === 0 && l2 === 0) {
+//             return headNode.next = 0;
+//         }
+//         if(l1) {
+//             val1 = l1.val;
+//             l1 = l1.next;
+//         }
+//         if(l2) {
+//             val2 = l2.val;
+//             l2 = l2.next;
+//         }
+//         let sum = val1 + val2 + temp;
+//         if (sum>=10) {
+//             temp = 1;
+//             const newSum = sum - 10;
+//             const currNode = new ListNode(newSum);
+//             newNode.next = currNode;
+//             newNode = currNode;
+//         }
+//         if (sum < 10) {
+//            const currNode = new ListNode(sum);
+//            temp = 0;
+//            newNode.next = currNode;
+//            newNode = currNode;
+//         }
+//         if (l1 === null && l2 === null) {
+//             if(temp) {
+//             const currNode1 = new ListNode(1);
+//             newNode.next = currNode1;
+//             newNode = currNode1;
+//             return headNode.next
+//             }
+//             return headNode.next
+//         }
+//     }
+//     return headNode.next;
+// }
